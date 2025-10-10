0a4eb36a3bf5fafca2d7a99a8bc1fab5c627c073
Jason Lewis <jasonedlewis@gmail.com>
2025-10-10 13:36:51 -0400

    main continue with  event class

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -39,12 +39,20 @@ def do_run_migrations(connection: Connection) -> None:
     )
     with context.begin_transaction():
         context.run_migrations()
-async def run_migrations_online() -> None:
+        
+async def run_async_migrations() -> None:
+    """Run migrations in 'online' mode with async support.""" 
     connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
+    config.get_section(config.config_ini_section, {}), prefix="sqlalchemy.",
+    poolclass=pool.NullPool,
     )
-    async with connectable.connect() as connection: 
-        await connection.run_sync(do_run_migrations)
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations) 
     await connectable.dispose()
+
+async def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.""" 
+    asyncio.run(run_async_migrations())
+    if context.is_offline_mode(): 
+        run_migrations_offline()
+    else: run_migrations_online()
