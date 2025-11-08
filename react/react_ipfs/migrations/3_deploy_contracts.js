5ef7b6ba0e8c86cb62c75005fb857cd1a6493daa
Jason Lewis <jasonedlewis@gmail.com>
2025-11-08 15:22:52 -0500

    Main added aws terraform start

--- react/react_ipfs/migrations/3_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-var Contract = artifacts.require("./AlbumNft.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Contract,"QmTttgz1gQwiyzt76ayfq7mCW39igtcTGJGooNorsrbsnz", "Just the Facts", "The Factualist",  "June 13, 1972");
-
-};
